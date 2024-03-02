8667e0247318b7c334786ef1817e73d1409ab5cb
Muhammed Hussein Karimi <info@karimi.dev>
2024-03-02 15:23:03 +0330

    ✨ feat: log level config and more logging
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- main.go
+++ main.go
@@ -5,17 +5,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/ilyakaznacheev/cleanenv"
-	_ "go.uber.org/automaxprocs"
-	"go.uber.org/zap"
 	"io"
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"net/http"
 	"net/url"
 	"strings"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	_ "go.uber.org/automaxprocs"
+	"go.uber.org/zap"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
@@ -24,14 +25,15 @@ import (
 )
 
 var (
-	ArvanCloudAPIBaseURL *url.URL = nil
-	logger, loggerErr             = zap.NewProduction()
+	ArvanCloudAPIBaseURL *url.URL    = nil
+	logger               *zap.Logger = nil
 )
 
 type (
 	arvanCloudDNSProviderGlobalConfig struct {
 		GroupName            string `env:"GROUP_NAME" env-default:"acme.parmin.cloud"`
 		ArvanCloudAPIBaseURL string `env:"ARVANCLOUD_API_BASE_URL" env-default:"https://napi.arvancloud.ir"`
+		LogLevel             string `env:"LOG_LEVEL" env-default:"INFO"`
 	}
 	DNSRecord struct {
 		ID    string            `json:"id,omitempty"`
@@ -48,11 +50,6 @@ type (
 )
 
 func main() {
-	if loggerErr != nil {
-		panic(loggerErr)
-	}
-	defer logger.Sync()
-
 	var cfg arvanCloudDNSProviderGlobalConfig
 
 	err := cleanenv.ReadEnv(&cfg)
@@ -61,11 +58,24 @@ func main() {
 	}
 	cfg.ArvanCloudAPIBaseURL = strings.TrimSuffix(cfg.ArvanCloudAPIBaseURL, "/")
 
+	logLevel, err := zap.ParseAtomicLevel(cfg.LogLevel)
+	if err != nil {
+		panic(fmt.Sprintf("Invalid Log Level: %v", err.Error()))
+	}
+	zapCfg := zap.NewProductionConfig()
+	zapCfg.Level = logLevel
+	var loggerErr error
+	logger, loggerErr = zapCfg.Build()
+	if loggerErr != nil {
+		panic(loggerErr)
+	}
+
+	defer logger.Sync()
+
 	ArvanCloudAPIBaseURL, err = url.ParseRequestURI(cfg.ArvanCloudAPIBaseURL)
 	if err != nil {
 		panic(err)
 	}
-
 	cmd.RunWebhookServer(cfg.GroupName,
 		&arvanCloudDNSProviderSolver{},
 	)
@@ -115,10 +125,24 @@ func (c *arvanCloudDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) err
 	if err != nil {
 		return err
 	}
+	sugar.Infow(
+		"Persisting record in ArvanCloud",
+		"Name", name,
+		"Type", "TXT",
+		"Value", ch.Key,
+	)
+
 	var records DNSRecords
 
 	_, err = c.SendAPIRequest("GET", "/cdn/4.0/domains/"+strings.TrimSuffix(ch.ResolvedZone, ".")+"/dns-records?type=txt&per_page=25&page=1&search="+name, apiKey, nil, &records)
 	if err != nil {
+		sugar.Errorw(
+			"Error while getting records from ArvanCloud",
+			"Name", name,
+			"Type", "TXT",
+			"Value", ch.Key,
+			"Error", err.Error(),
+		)
 		return err
 	}
 	for _, record := range records.Data {
@@ -144,9 +168,24 @@ func (c *arvanCloudDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) err
 	var createResponseBody any
 	createResponse, err := c.SendAPIRequest("POST", "/cdn/4.0/domains/"+strings.TrimSuffix(ch.ResolvedZone, ".")+"/dns-records", apiKey, record, &createResponseBody)
 	if err != nil {
+		sugar.Errorw(
+			"Error while creating record in ArvanCloud",
+			"Name", name,
+			"Type", "TXT",
+			"Value", ch.Key,
+			"Error", err.Error(),
+		)
 		return err
 	}
 	if createResponse.StatusCode != http.StatusCreated {
+		sugar.Errorw(
+			"API error while getting records from ArvanCloud",
+			"Name", name,
+			"Type", "TXT",
+			"Value", ch.Key,
+			"Status", createResponse.Status,
+			"Body", createResponseBody,
+		)
 		return fmt.Errorf("Error while creating records in ArvanCloud, Status: %v, Body: %+v", createResponse.Status, createResponseBody)
 	}
 	sugar.Infow(
@@ -170,11 +209,23 @@ func (c *arvanCloudDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) err
 	if err != nil {
 		return err
 	}
-
+	sugar.Infow(
+		"Cleaning up record in ArvanCloud",
+		"Name", name,
+		"Type", "TXT",
+		"Value", ch.Key,
+	)
 	var records DNSRecords
 
 	_, err = c.SendAPIRequest("GET", "/cdn/4.0/domains/"+strings.TrimSuffix(ch.ResolvedZone, ".")+"/dns-records?type=txt&per_page=25&page=1&search="+name, apiKey, nil, &records)
 	if err != nil {
+		sugar.Errorw(
+			"Error while getting records from ArvanCloud",
+			"Name", name,
+			"Type", "TXT",
+			"Value", ch.Key,
+			"Error", err.Error(),
+		)
 		return err
 	}
 
@@ -183,11 +234,32 @@ func (c *arvanCloudDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) err
 			var cleanUpResponseBody any
 			response, err := c.SendAPIRequest("DELETE", "/cdn/4.0/domains/"+strings.TrimSuffix(ch.ResolvedZone, ".")+"/dns-records/"+record.ID, apiKey, nil, cleanUpResponseBody)
 			if err != nil {
+				sugar.Errorw(
+					"Error while cleaning up records from ArvanCloud",
+					"Name", name,
+					"Type", "TXT",
+					"Value", ch.Key,
+					"Error", err.Error(),
+				)
 				return err
 			}
 			if response.StatusCode != http.StatusOK {
-				return fmt.Errorf("Error while cleaning up records from ArvanCloud, Status: %v, Body: %+v", response.Status, cleanUpResponseBody)
+				sugar.Errorw(
+					"API error while cleaning up records from ArvanCloud",
+					"Name", name,
+					"Type", "TXT",
+					"Value", ch.Key,
+					"Status", response.Status,
+					"Body", cleanUpResponseBody,
+				)
+				return fmt.Errorf("API error while cleaning up records from ArvanCloud, Status: %v, Body: %+v", response.Status, cleanUpResponseBody)
 			}
+			sugar.Infow(
+				"Record cleaned up in ArvanCloud",
+				"Name", name,
+				"Type", "TXT",
+				"Value", ch.Key,
+			)
 			return nil
 		}
 	}
@@ -201,7 +273,9 @@ func (c *arvanCloudDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) err
 }
 
 func (c *arvanCloudDNSProviderSolver) SendAPIRequest(method, uri, token string, requestBody any, responseBody any) (*http.Response, error) {
-	if len(strings.Split(token, " ")) != 2 {
+	sugar := logger.Sugar()
+	tokenParts := strings.Split(token, " ")
+	if len(tokenParts) != 2 {
 		return nil, fmt.Errorf("Token is not valid, it should be in two parts")
 	}
 	rel := &url.URL{Path: uri}
@@ -224,10 +298,40 @@ func (c *arvanCloudDNSProviderSolver) SendAPIRequest(method, uri, token string, 
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Authorization", token)
 	req.Header.Set("User-Agent", "ArvanCloud Issuer Webhook for CertManager")
+	filteredHeaders := make(map[string]string)
+	curlHeaders := ""
+	for k, v := range req.Header {
+		if k == "Authorization" {
+			filteredHeaders[k] = tokenParts[0] + " " + strings.Repeat("#", len(tokenParts[1]))
+			continue
+		}
+		curlHeaders += fmt.Sprintf(` -H %q`, fmt.Sprintf("%s: %s", k, v[0]))
+		filteredHeaders[k] = v[0]
+	}
+	curlOpts := curlHeaders
+	if requestBody != nil {
+		body, _ := json.Marshal(requestBody)
+		curlOpts += fmt.Sprintf(" -d %s", string(body))
+	}
+	sugar.Debugw(
+		"Sending request to ArvanCloud API",
+		"Headers", filteredHeaders,
+		"Body", fmt.Sprintf("%+v", requestBody),
+		"URL", u.String(),
+		"cURL", fmt.Sprintf("curl -v -X %s %s '%s'", req.Method, curlOpts, u.String()),
+	)
+	startTime := time.Now()
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	latency := startTime.Sub(time.Now())
+	sugar.Debugw(
+		"Request sent to ArvanCloud API",
+		"URL", u.String(),
+		"Status", resp.Status,
+		"Took", latency.String(),
+	)
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(responseBody)
 	return resp, err
